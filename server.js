e68b996333bce8c5c8c86320e49054f497abe779
itaytester <itaytester@gmail.com>
2021-09-02 19:54:26 +0300

    fixed 4

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const SERVER_PORT = process.env.PORT || 8080;
 const express = require("express");
 const ffmpeg = require("ffmpeg");
-const dotenv = require("dotenv");
 const path = require("path");
 
 const app = express();
-dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
